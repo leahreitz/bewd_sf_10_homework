ea563eaf7af62de4b6005ca0a589738f7e7c420b
Anne Garland Berry <annegarland.berry@gmail.com>
2015-09-02 18:55:32 -0700

    adding hwk_10

--- AnneGarlandBerry-annegarlandberry/hw_7/real_estate.rb
+++ AnneGarlandBerry-annegarlandberry/hw_7/real_estate.rb
@@ -5,34 +5,42 @@ require_relative 'lib/renter'
 require 'pry'
 
 def create_building
-  puts "New building" ##gets user input
-  puts "What is the address of the building?"
-   address = gets.strip
+  puts "New building"
+  puts "What is the building name? \n"
+  bldg_name = gets.strip
 
-   puts "What is the name of building?"
-   building_name = gets.strip
-   building = Building.new(address, building_name) ##must define variable here because we are initiating
+  puts "What is the building address? \n"
+  address = gets.strip
+  building = Building.new(address, bldg_name)
+  add_units(building)
+end
 
-   puts "How many units?"
-   num_apts = gets.strip.to_i ##user input determines number
-   building.apartments = Apartment.apartment_creator(num_apts)
+def add_units(building)
+  puts "How many units? \n" ##putting a binding pry will help look at what we are passing in to 'unit"'
+  units = gets.strip.to_i ##figure out how to create an apartment three times
+  units.times do
+    building.apartment.push(create_apartment)
+  end
+  building.apt_names
+end
 
-   building.apartments.each do |apartment| ##randomizing rather than letting user do this
-     num = Random.rand(0…10) ##running random counter to generate instances of apartment
-     if num <= 8
-       apartment.tenant = Tenant.random_tenant_generator
-     end
-   end
-   puts building ##building already exists now and the variable is defined
-   building.apartments.each do |apartment|
-     puts apartment
-     apartment.print_tenant_info
-    end
- end
+def create_apartment
+  puts "New apartment"
+  puts "What is the name of the apartment? \n"
+  apt_name = gets.strip
 
+  puts "How many sqft?"
+  sqft = gets.strip
 
+  puts "How many bedrooms?"
+  bedrooms = gets.strip
 
+  puts "How many bathrooms?"
+  bathrooms = gets.strip
 
+apartment =  Apartment.new(apt_name, sqft, bedrooms, bathrooms)
+ puts add_renter(apartment)
+end
 
 
 
