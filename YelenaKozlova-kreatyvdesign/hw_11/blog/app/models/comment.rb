a206578e2ae2e28b1b647de2cd687dfc316b74b7
ykozlova <yelena.kozlova@druva.com>
2015-09-13 01:19:16 -0700

    Adding homework 11

---
+++ YelenaKozlova-kreatyvdesign/hw_11/blog/app/models/comment.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  commenter  :string
+#  body       :text
+#  article_id :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Comment < ActiveRecord::Base
+  belongs_to :article
+  validates :commenter, presence: true
+end
